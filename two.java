4dbe4d5dab3705af5f4e2239045b5fdb47ef44b1
zergreen <kawhao101@gmail.com>
2022-02-13 12:36:06 +0700

    add 4 File to => Zero Round of CoedArcade 13/2/2022

---
+++ two.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class TB {
+    public static void main(String[] args) {
+        // double tb = 3.005;
+        // // double tb = 1.5;
+        // // double tb = 3.005;
+        // int money = 0;
+        // double mod = tb%1000.0;
+        System.out.println(5.5%0.10);
+        // if(tb < 1) {
+        //     money += 500; 
+        // }
+        // if(tb > 1 && tb < 2) {
+            
+        // }
+
+        
+
+    }
+}
