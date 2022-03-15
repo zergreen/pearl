c42f1941ab3d5cf2e41fc4ba47ffa5f3b8ec2613
zergreen <kawhao101@gmail.com>
2022-03-16 02:02:46 +0700

    Added : ROUND2 directory

---
+++ hello.java
@@ -0,0 +1,21 @@
+class hello {
+    public static void main(String[] args){
+        System.out.println("helloWOrld");
+        System.out.println("helloWOrld");
+        System.out.println("helloWOrld");
+        System.out.println("helloWOrld");
+        System.out.println("helloWOrld");
+        System.out.println("helloWOrld");
+        System.out.println("helloWOrld");
+        System.out.println("helloWOrld");
+        System.out.println("helloWOrld");
+        System.out.println("helloWOrld");
+        System.out.println("helloWOrld");
+        System.out.println("helloWOrld");
+        System.out.println("helloWOrld");
+        System.out.println("helloWOrld");
+        System.out.println("helloWOrld");
+        System.out.println("helloWOrld");
+        System.out.println("helloWOrld");
+    }
+}
